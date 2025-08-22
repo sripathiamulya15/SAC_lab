7e5e0e04dfe501d6be1579c16d9d8956a4a38a46
S.Amulya <sripathiamulya15@gmail.com>
2025-08-22 13:06:54 +0530

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,15 @@
+#name=input("enter your name:")
+#print("welcome",name)
+#arithmetic operation
+'''+-addition
+- means subtraction
+* multiply'''
+a=float(input("enter the value of a:"))
+b=float(input("enter the value of b:"))
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("exponent",a**2)
+print("floor division",a//2)
+print("modulus operation",a%2)
+print("division",a/b)
