efa0fa7f67d4ca889d6175b324c715adeeffea37
S.Amulya <sripathiamulya15@gmail.com>
2025-09-30 18:31:24 +0530

    Add files via upload

---
+++ primewithcmd.py
@@ -0,0 +1,17 @@
+#prime number using command line input
+import sys
+num=int(sys.argv[1])
+if num<=1:
+    print(f"{num} is not a prime number")
+elif num==2 or num==3:
+    print(f"{num} is a prime number")
+else:
+    is_prime=True
+    for i in range(2,int(num**0.5)+1):
+        if num%i==0:
+            is_prime=False
+            break
+    if is_prime:
+            print(f"{num} is a prime number")
+    else:
+            print(f"{num} is not a prime number")
